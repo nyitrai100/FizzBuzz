d85a5de691ff5056f4ac6a5403b5cac134075564
nyitraibence <nyitrai100@gmail.com>
2024-05-03 08:16:37 +0100

    done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class  Program
+{
+    //Create a for loop from 1 to X (15)
+    //3 and 5 = FizzBuzz
+    //3 = Fizz
+    //5 = Buzz
+    // else = number
+    
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 15; i++)
+        {
+            if ((i % 5) == 0 && (i % 3) == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if((i % 3) == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if((i % 5)== 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
